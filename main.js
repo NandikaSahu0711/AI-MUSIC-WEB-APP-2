b61278bbf01f796a60b57ce6ad770559120a9978
NandikaSahu0711 <79842952+NandikaSahu0711@users.noreply.github.com>
2022-01-15 13:13:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+song1="";
+song1="";
+
+function preload(){
+    song1 = loadSound("music.mp3");
+    song2 = loadSound("music2.mp3");
+}
+
+function setup(){
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw(){
+    image(video, 100, 100, 500, 400);
+}
+
+function play(){
+    song.play();
+}
